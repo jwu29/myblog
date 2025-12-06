03571b3c1413356efa9851f770fc081368cd83a3
jwu29 <josiahwu29@gmail.com>
2025-12-06 13:25:59 +1100

    fix on vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.md', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
-})
+  assetsInclude: [
+    "**/*.md",
+    "**/*.png",
+    "**/*.jpg",
+    "**/*.jpeg",
+    "**/*.gif",
+    "**/*.svg",
+  ],
+  optimizeDeps: {
+    include: ["remark-gfm"],
+  },
+});
